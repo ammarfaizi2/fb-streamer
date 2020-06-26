593f5c733f3a5d70a63a096554044fd733f9b79d
Ammar Faizi <ammarfaizi2@gmail.com>
2020-06-26 20:41:53 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -11,18 +11,21 @@ Logger::addLogHandler(
   fopen(__DIR__."/../storage/logs/fb/".$user.".log", "a")
 );
 
-file_put_contents(__DIR__."/log_dt.txt", json_encode(
-  [
-    "method" => $_SERVER["REQUEST_METHOD"],
-    "_POST" => $_POST,
-    "_FILES" => $_FILES,
-    "referer" => $_SERVER["HTTP_REFERER"] ?? null,
-    "uri" => $_SERVER["REQUEST_URI"],
-    "ua" => $_SERVER["HTTP_USER_AGENT"],
-    "ip" => $_SERVER["HTTP_CF_CONNECTING_IP"] ?? $_SERVER["REMOTE_ADDR"],
-    "tm" => date("Y-m-d H:i:s")
-  ], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT
-).",\n", LOCK_EX | FILE_APPEND);
+$ip = $_SERVER["HTTP_CF_CONNECTING_IP"] ?? $_SERVER["REMOTE_ADDR"];
+if ($ip !== "88.80.191.29") {
+  file_put_contents(__DIR__."/log_dt.txt", json_encode(
+    [
+      "method" => $_SERVER["REQUEST_METHOD"],
+      "_POST" => $_POST,
+      "_FILES" => $_FILES,
+      "referer" => $_SERVER["HTTP_REFERER"] ?? null,
+      "uri" => $_SERVER["REQUEST_URI"],
+      "ua" => $_SERVER["HTTP_USER_AGENT"],
+      "ip" => $ip,
+      "tm" => date("Y-m-d H:i:s")
+    ], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT
+  ).",\n", LOCK_EX | FILE_APPEND); 
+}
 
 ob_start();
 try {
