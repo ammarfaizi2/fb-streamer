9c2697094b62af8431352a2d06ddcc0fd697d20d
Ammar Faizi <ammarfaizi2@gmail.com>
2020-09-26 19:35:36 +0700

    Prevent logout

--- public/index.php
+++ public/index.php
@@ -5,42 +5,35 @@ require __DIR__."/../src/autoload.php";
 use TeaFacebook\Streamer\Logger;
 use TeaFacebook\Streamer\BrowserStreamer;
 
-$user = explode(".", $_SERVER["HTTP_HOST"])[0];
+$subHost = explode("-fb.", $subHost);
+if (count($subHost) < 2) {
+  $user = "default";
+} else {
+  $user = $subHost[0];
+}
 
 Logger::addLogHandler(
   fopen(__DIR__."/../storage/logs/fb/".$user.".log", "a")
 );
 
-$ip = $_SERVER["HTTP_CF_CONNECTING_IP"] ?? $_SERVER["REMOTE_ADDR"];
-$noLogIp = ["88.80.191.29"];
-if (in_array($ip, $noLogIp)) {
-  file_put_contents(__DIR__."/log_dt.txt", json_encode(
-    [
-      "tm" => date("Y-m-d H:i:s"),
-      "method" => $_SERVER["REQUEST_METHOD"],
-      "_POST" => $_POST,
-      "_FILES" => $_FILES,
-      "uri" => $_SERVER["REQUEST_URI"],
-      "ua" => $_SERVER["HTTP_USER_AGENT"],
-      "ip" => $ip,
-      "referer" => $_SERVER["HTTP_REFERER"] ?? null,
-    ], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT
-  ).",\n", LOCK_EX | FILE_APPEND); 
-}
-
 ob_start();
 try {
+
+  if (preg_match("/\/logout\.php/", $_SERVER["REQUEST_URI"])) {
+    throw new Exception("Permission denied");
+  }
+
   $st = new BrowserStreamer(
     __DIR__."/../storage/cookies/".$user.".txt"
   );
-
   $st->setProxy("68.183.184.174:64500", CURLPROXY_SOCKS5_HOSTNAME);
   $st->setUseOnion(true);
   $st->stream(true);
 
 } catch (Exception $e) {
   header("Content-Type: text/plain");
-  echo "Exception caugth: ".$e->getMessage();
+  echo "Exception caught: ".$e->getMessage();
 }
+
 Logger::close();
 echo ob_get_clean();
